1d74c292dc671a4e210c87365214f1d17329010a
Carlos C. Romero Pertuz <romeropertuzcarlos@gmail.com>
2021-02-16 04:06:00 +0100

    Agregar archivos de proyecto.

---
+++ NetCoreAPI/Data/NetCoreAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetCoreAPI.Models;
+
+namespace NetCoreAPI.Data
+{
+    public class NetCoreAPIContext : DbContext
+    {
+        public NetCoreAPIContext (DbContextOptions<NetCoreAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetCoreAPI.Models.Usuario> Usuario { get; set; }
+    }
+}
---
+++ NetCoreAPI/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetCoreAPI.Data;
+
+namespace NetCoreAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NetCoreAPI", Version = "v1" });
+            });
+
+            services.AddDbContext<NetCoreAPIContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("NetCoreAPIContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(builds => { builds.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NetCoreAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
